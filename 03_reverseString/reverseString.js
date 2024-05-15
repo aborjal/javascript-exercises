2abb0fa690985ac080ad3a7647d2bfe445fcbc1f
aborjal <166768400+aborjal@users.noreply.github.com>
2024-05-15 18:29:07 -0500

    Edit reverseString.js with solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    /* STEPS
+    1.-TAKE IN A STRING
+    2.-EXTRACT THE LAST LETTER OF THE STRING
+    3.-THAT LETTER IS ADD TO A NEW EMPTY STRING
+    4.-REPEAT THE PROCESS WITH ALL THE LETTERS OF THE STRING
+    5.- RETURN THE NEW WORD
+    
+    */
+    let i = 0;
+    let reverseStr = "";
 
-};
+    while (i<string.length){
+        let lastLetterString = Array.from(string);
+        reverseStr += lastLetterString[string.length-(i+1)];
+        i++; 
+    };
+
+    return reverseStr;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
