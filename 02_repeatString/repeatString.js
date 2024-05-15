2fc0479059d6a89971873e7d5a48854098fca1b7
aborjal <166768400+aborjal@users.noreply.github.com>
2024-05-15 16:41:18 -0500

    Edit repeatString.js with solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0;
 
+    words = "";
+
+    if(num<0){
+        return "ERROR";
+    } else {
+        while (i < num) {
+            words += string;
+            i++;
+        }
+        return words;
+    }
+    
 };
 
 // Do not edit below this line
