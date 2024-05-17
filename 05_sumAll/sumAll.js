f229a866041659135e6c376a9bb49d16fc40dea2
aborjal <166768400+aborjal@users.noreply.github.com>
2024-05-16 19:56:55 -0500

    Edit exercise 5 with solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let i;
+    let lastNumber;
+    let sum = 0;
+
+    if(num1 < num2) {
+        i= num1;
+        lastNumber = num2;
+    } else {
+        i= num2;
+        lastNumber = num1;
+    }
+    
+    if(num1>0 && num2>0 && typeof(num1)=== "number" && typeof(num2)=== "number") {
+        while(i <=  lastNumber){
+            sum += i ;
+            i++;
+        }
+
+        return sum;
+    } else{
+        return "ERROR";
+    }
+     
+
+    
 };
 
 // Do not edit below this line
