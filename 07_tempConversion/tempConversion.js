9d8ba7fefede68136b6273c03c79b1abd1e8af03
aborjal <166768400+aborjal@users.noreply.github.com>
2024-05-16 20:25:38 -0500

    Edit tempCOnversion.js with solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+ const celsius = (temperature - 32) * (5/9);
+
+ if(celsius % 1 !== 0){
+  return Number(celsius.toFixed(1));
+} else {
+  return celsius;
+}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheit = (temperature * (9/5)) + 32;
+
+  if(fahrenheit % 1 !== 0){
+    return Number(fahrenheit.toFixed(1));
+  } else {
+    return fahrenheit;
+  }
 };
 
 // Do not edit below this line
